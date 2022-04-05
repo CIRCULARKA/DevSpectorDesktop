e919ca8641c6d457602e1317c971497b21eee993
CIRCULARKA <donkardon@list.ru>
2022-04-05 11:25:47 +0500

    Implement MessagesBroker

---
+++ src/DevSpector.Service/MessagesBroker/MessagesBroker.cs
@@ -0,0 +1,17 @@
+﻿namespace DevSpector.Desktop.Service
+{
+    public class MessagesBroker
+    {
+        private readonly IApplicationEvents _appEvents;
+
+        public MessagesBroker(IApplicationEvents events)
+        {
+            _appEvents = events;
+        }
+
+        public void NotifyUser(string message)
+        {
+            _appEvents.RaiseUserNotified(message);
+        }
+    }
+}
