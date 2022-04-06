6d7caa8627fcdf9ae9c3b4acde5911b59220eca8
CIRCULARKA <donkardon@list.ru>
2022-04-06 21:55:08 +0500

    Make it possible to remove device software

--- src/DevSpector.UI/ViewModels/DevicesSection/DeviceInfo/Implementations/SoftwareInfoViewModel.cs
+++ src/DevSpector.UI/ViewModels/DevicesSection/DeviceInfo/Implementations/SoftwareInfoViewModel.cs
@@ -1,3 +1,6 @@
+using System;
+using System.Reactive;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using ReactiveUI;
 using DevSpector.SDK.Models;
@@ -9,23 +12,42 @@ namespace DevSpector.Desktop.UI.ViewModels
     {
         private List<Software> _software;
 
+        private Software _selectedSoftware;
+
         private readonly IDevicesStorage _storage;
 
         private readonly IDevicesListViewModel _devicesListViewModel;
 
+        private readonly IMessagesBroker _messagesBroker;
+
         public SoftwareInfoViewModel(
             IDevicesStorage storage,
-            IDevicesListViewModel devicesListViewModel
+            IDevicesListViewModel devicesListViewModel,
+            IMessagesBroker messagesBroker
         )
         {
             _storage = storage;
             _devicesListViewModel = devicesListViewModel;
+
+            _messagesBroker = messagesBroker;
+
+            RemoveSoftwareCommand = ReactiveCommand.CreateFromTask(
+                RemoveSoftware
+            );
         }
+
+        public ReactiveCommand<Unit, Unit> RemoveSoftwareCommand { get; }
 
         public List<Software> Software
         {
             get => _software;
             set => this.RaiseAndSetIfChanged(ref _software, value);
+        }
+
+        public Software SelectedSoftware
+        {
+            get => _selectedSoftware;
+            set => this.RaiseAndSetIfChanged(ref _selectedSoftware, value);
         }
 
         public void UpdateDeviceInfo(Device target)
@@ -34,6 +56,30 @@ namespace DevSpector.Desktop.UI.ViewModels
                 Software = null;
             else
                 Software = target.Software;
+        }
+
+        public async Task RemoveSoftware()
+        {
+            try
+            {
+                Device selectedDevice = _devicesListViewModel.SelectedItem;
+
+                await _storage.RemoveSoftwareAsync(selectedDevice.InventoryNumber, _selectedSoftware);
+
+                Software.Remove(SelectedSoftware);
+
+                Software removedSoftware = SelectedSoftware;
+
+                var temp = Software;
+                Software = null;
+                Software = temp;
+
+                _messagesBroker.NotifyUser($"ПО \"{removedSoftware.SoftwareName}\" удалено");
+            }
+            catch (Exception e)
+            {
+                _messagesBroker.NotifyUser(e.Message);
+            }
         }
     }
 }
