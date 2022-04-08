c0fc8f1160c9e82223363e4dd7c8e4a831a400a6
CIRCULARKA <donkardon@list.ru>
2022-04-08 13:59:38 +0500

    Add new method to load free IP addresses

--- src/DevSpector.UI/ViewModels/DevicesSection/DeviceInfo/Implementations/NetworkInfoViewModel.cs
+++ src/DevSpector.UI/ViewModels/DevicesSection/DeviceInfo/Implementations/NetworkInfoViewModel.cs
@@ -1,11 +1,45 @@
+using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using ReactiveUI;
 using DevSpector.SDK.Models;
+using DevSpector.Desktop.Service;
 
 namespace DevSpector.Desktop.UI.ViewModels
 {
     public class NetworkInfoViewModel : ListViewModelBase<string>, INetworkInfoViewModel
     {
-        public NetworkInfoViewModel() { }
+        private ObservableCollection<string> _freeIP;
+
+        private string _selectedFreeIP;
+
+        private readonly IDevicesStorage _storage;
+
+        private readonly IMessagesBroker _messagesBroker;
+
+        private readonly IDevicesListViewModel _devicesListViewModel;
+
+        public NetworkInfoViewModel(
+            IDevicesStorage storage,
+            IMessagesBroker messagesBroker,
+            IDevicesListViewModel devicesListViewModel
+        )
+        {
+            _freeIP = new ObservableCollection<string>();
+
+            _storage = storage;
+            _messagesBroker = messagesBroker;
+            devicesListViewModel = _devicesListViewModel;
+        }
+
+        public ObservableCollection<string> FreeIP => _freeIP;
+
+        public string SelectedFreeIP
+        {
+            get => _selectedFreeIP;
+            set => this.RaiseAndSetIfChanged(ref _selectedFreeIP, value);
+        }
 
         public override string SelectedItem
         {
@@ -20,6 +54,24 @@ namespace DevSpector.Desktop.UI.ViewModels
             Items.Clear();
             foreach (var ip in target.IPAddresses)
                 Items.Add(ip);
+        }
+
+        public async Task LoadFreeIP()
+        {
+            try
+            {
+                List<string> updatedList = await _storage.GetFreeIP();
+
+                await Task.Run(() => {
+                    FreeIP.Clear();
+                    foreach (var ip in updatedList)
+                        FreeIP.Add(ip);
+                });
+            }
+            catch (Exception e)
+            {
+                _messagesBroker.NotifyUser(e.Message);
+            }
         }
     }
 }
